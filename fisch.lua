09ee508e49c61490a2f9b3e8097c7742dec970e2
Scripter999436 <olus641@gmail.com>
2024-12-22 21:04:06 +0700

    Update fisch.lua

--- fisch.lua
+++ fisch.lua
@@ -8,9 +8,17 @@ game:GetService("Players").LocalPlayer.Character:FindFirstChild("Rod Of The Dept
 while true do wait ()
 local args = {
     [1] = 100,
-    [2] = ture
+    [2] = fasle
 }
 
 game:GetService("ReplicatedStorage").events.reelfinished:FireServer(unpack(args))
+local args = {
+    [1] = 21.399999999999995,
+    [2] = 1
+}
+while true do wait ()
+game:GetService("Players").LocalPlayer.Character:FindFirstChild("Rod Of The Depths").events.cast:FireServer(unpack(args))
+        end
+    end
 end
-end
+    
